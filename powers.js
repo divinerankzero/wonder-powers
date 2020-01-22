1be915462d47e6adef49cc9a1960374b0a1db643
Keith Potempa <keithrpotempa@gmail.com>
2020-01-22 14:57:45 -0600

    completed exercise, but refactoring

--- powers.js
+++ powers.js
@@ -1,41 +1,61 @@
 // Event Handler Functions
-const flightHandlerFunction = (event) => {
-    sectionFlight.classList.remove("disabled");
-    sectionFlight.classList.add("enabled");
+const handlerFunction = (event) => {
+    const power = event.target.id.split("-")[1];
+    if (power === "all") {
+        const state = event.target.id.split("-")[0];
+        if (state === "activate") {
+            sectionsAllPowers.forEach(power => {
+                power.classList.remove("disabled");
+                power.classList.add("enabled");
+            })
+        } else if (state === "deactivate") {
+            sectionsAllPowers.forEach(power => {
+                power.classList.remove("enabled");
+                power.classList.add("disabled");
+            })
+        }
+    } else {
+        const section = document.querySelector(`#${power}`)
+        section.classList.remove("disabled");
+        section.classList.add("enabled");
+    }
 }
 
-const mindReaderHandlerFunction = (event) => {
-    sectionMindReading.classList.remove("disabled");
-    sectionMindReading.classList.add("enabled");
-}
+// NON-DRY Practice Run
+// const flightHandlerFunction = (event) => {
+//     sectionFlight.classList.remove("disabled");
+//     sectionFlight.classList.add("enabled");
+// }
 
-const xRayHandlerFunction = (event) => {
-    sectionXRay.classList.remove("disabled");
-    sectionXRay.classList.add("enabled");
-}
+// const mindReaderHandlerFunction = (event) => {
+//     sectionMindReading.classList.remove("disabled");
+//     sectionMindReading.classList.add("enabled");
+// }
 
-const activateAllHandlerFunction = (event) => {
-    sectionsAllPowers.forEach(power => {
-        power.classList.remove("disabled");
-        power.classList.add("enabled");
-    })
-}
+// const xRayHandlerFunction = (event) => {
+//     sectionXRay.classList.remove("disabled");
+//     sectionXRay.classList.add("enabled");
+// }
 
-const deactivateAllHandlerFunction = (event) => {
-    sectionsAllPowers.forEach(power => {
-        power.classList.remove("enabled");
-        power.classList.add("disabled");
-    })
-}
+// const activateAllHandlerFunction = (event) => {
+//     sectionsAllPowers.forEach(power => {
+//         power.classList.remove("disabled");
+//         power.classList.add("enabled");
+//     })
+// }
+
+// const deactivateAllHandlerFunction = (event) => {
+//     sectionsAllPowers.forEach(power => {
+//         power.classList.remove("enabled");
+//         power.classList.add("disabled");
+//     })
+// }
 
 // DOM Sections
 const sectionFlight = document.querySelector("#flight");
 const sectionMindReading = document.querySelector("#mindreading")
 const sectionXRay = document.querySelector("#xray");
 const sectionsAllPowers = document.querySelectorAll(".power")
-
-
-console.log("All Powers: ", sectionsAllPowers)
 
 // DOM Buttons
 const activateFlightButton = document.querySelector("#activate-flight")
@@ -45,9 +65,16 @@ const activateAllButton = document.querySelector("#activate-all");
 const deactivateAllButton = document.querySelector("#deactivate-all");
 
 // Event Listeners
-activateFlightButton.addEventListener("click", flightHandlerFunction)
-activateMindReadingButton.addEventListener("click", mindReaderHandlerFunction)
-activateXRayButton.addEventListener("click", xRayHandlerFunction)
-activateAllButton.addEventListener("click", activateAllHandlerFunction)
-deactivateAllButton.addEventListener("click", deactivateAllHandlerFunction)
+activateFlightButton.addEventListener("click", handlerFunction)
+activateMindReadingButton.addEventListener("click", handlerFunction)
+activateXRayButton.addEventListener("click", handlerFunction)
+activateAllButton.addEventListener("click", handlerFunction)
+deactivateAllButton.addEventListener("click", handlerFunction)
+
+// NON-DRY Practice Run
+// activateFlightButton.addEventListener("click", flightHandlerFunction)
+// activateMindReadingButton.addEventListener("click", mindReaderHandlerFunction)
+// activateXRayButton.addEventListener("click", xRayHandlerFunction)
+// activateAllButton.addEventListener("click", activateAllHandlerFunction)
+// deactivateAllButton.addEventListener("click", deactivateAllHandlerFunction)
 
